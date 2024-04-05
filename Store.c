8c73c58e162d49d168b717056ca74c7d4217af95
Jairo <jvegag@uamv.edu.ni>
2024-04-04 18:32:08 -0600

    Primer Commit

---
+++ Store.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+    float amount, price, totalPrice, discount, finalPrice;
+
+    printf("Enter how much products are you buying\n");
+    scanf("%f", &amount);
+
+    printf("Enter the price\n");
+    scanf("%f", &price);
+
+
+    totalPrice = amount * price;
+    discount = totalPrice * 0.10;
+    finalPrice = totalPrice - discount;
+
+    printf("Total price before discount is: %.2f\n", totalPrice);
+    printf("Discount of 10 percent: %.2f\n", discount);
+    printf("Final price with discount: %.2f\n", finalPrice);
+
+
+    return 0;
+}
